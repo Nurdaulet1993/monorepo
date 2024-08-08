a205fb1042301694fc806840fdc6ddf1ff063e89
Nurdaulet <nurik93sat@gmail.com>
2024-08-09 00:30:49 +0500

    Refactor

--- libs/ui/src/lib/tabs/tab-item/tab-item.component.ts
+++ libs/ui/src/lib/tabs/tab-item/tab-item.component.ts
@@ -1,9 +1,4 @@
-import { Component, inject, input, OnInit, TemplateRef, viewChild } from '@angular/core';
-import { TabsComponent } from '../tabs.component';
-import { UniqueSelectionDispatcher } from '@angular/cdk/collections';
-
-let tabItemId = 0;
-
+import { Component, input, TemplateRef, viewChild } from '@angular/core';
 @Component({
   selector: 'ui-tab-item',
   standalone: true,
@@ -11,38 +6,7 @@ let tabItemId = 0;
   templateUrl: './tab-item.component.html',
   styleUrl: './tab-item.component.scss'
 })
-export class TabItemComponent implements OnInit {
-  id = tabItemId++;
+export class TabItemComponent {
   label = input.required<string>();
-  templateRef = viewChild(TemplateRef, { read: TemplateRef });
-  tabs = inject(TabsComponent);
-  private expansionDispatcher = inject(UniqueSelectionDispatcher, { skipSelf: true });
-
-  ngOnInit(): void {
-    const selectedIndex = this.tabs.selectedIndex();
-    console.log(selectedIndex)
-
-    console.log(this.tabs)
-    if (selectedIndex === this.id) {
-      this.tabs.currentTab = this.templateRef() ?? null;
-    }
-
-    // this.expansionDispatcher.listen(
-    //   (id: string, tabsId: string) => {
-    //     const selectedIndex = this.tabs.selectedIndex();
-    //
-    //     console.log('Hello')
-    //
-    //     if (this.tabs && this.tabs.id === tabsId && this.id !== id) {
-    //
-    //       if (selectedIndex.toString() === this.id.split('-')[1]) {
-    //         this.tabs.currentTab = this.templateRef() ?? null;
-    //       }
-    //
-    //     }
-    //
-    //
-    //   }
-    // )
-  }
+  templateRef = viewChild.required(TemplateRef);
 }
--- libs/ui/src/lib/tabs/tabs.component.ts
+++ libs/ui/src/lib/tabs/tabs.component.ts
@@ -1,30 +1,36 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  computed,
-  ContentChildren, HostBinding,
-  input,
-  QueryList,
-  TemplateRef
+  contentChildren,
+  Input, TemplateRef
 } from '@angular/core';
 import { TabItemComponent } from './tab-item/tab-item.component';
-import { NgTemplateOutlet } from '@angular/common';
+import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
+import { BehaviorSubject, map, Observable, scan } from 'rxjs';
 
-let tabsId = 0;
 
 @Component({
   selector: 'ui-tabs',
   standalone: true,
   imports: [
-    NgTemplateOutlet
+    NgTemplateOutlet,
+    AsyncPipe
   ],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TabsComponent {
-  selectedIndex = input<number>(0);
-  currentTab: TemplateRef<any> | null = null;
-  @ContentChildren(TabItemComponent) tabs!: QueryList<TabItemComponent>;
-  @HostBinding('attr.id') readonly id: string = `tabsItem-${tabsId++}`;
+  @Input() set selectedIndex(value: number | null) {
+    this.selectedIndexSub.next(value)
+  };
+  tabItems = contentChildren(TabItemComponent);
+  private readonly selectedIndexSub = new BehaviorSubject<number | null>(null);
+  selectedIndex$ = this.selectedIndexSub
+    .pipe(scan((value, newValue) => value === newValue ? null : newValue));
+  currentTabTemplate$: Observable<TemplateRef<any> | null> = this.selectedIndex$
+    .pipe(map(value => value === null ? null : this.tabItems()[value].templateRef()));
+  selectIndex(index: number): void {
+    this.selectedIndexSub.next(index);
+  }
 }
